d42a4765a1bb3d1dd3ea2dc7fc393f15420768d9
pvmend <mendoza.v.paul@gmail.com>
2023-04-25 09:06:19 -0700

    moved files

--- server.js
+++ server.js
@@ -94,14 +94,14 @@ app.delete('/api/notes/:id',(req,res)=>{
 
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 app.get('/notes', (req,res) =>{
-    res.sendFile(path.join(__dirname,'/notes.html'))
+    res.sendFile(path.join(__dirname,'/public/notes.html'))
 }
 )
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
