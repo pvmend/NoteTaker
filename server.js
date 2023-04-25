69f7fa2a818d587d604220d23e067927efc552f5
pvmend <mendoza.v.paul@gmail.com>
2023-04-25 07:47:49 -0700

    port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path')
 const fs = require('fs');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 let notes = require('./db/db.json');
 const uuid = require('./helpers/uuid');
 
